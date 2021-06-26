9da84dabe24c8f0d3cb5679a37c3471fe3989095
RishitaArora <rishita3099@gmail.com>
2021-06-26 15:42:20 +0400

    Development completed commit
    Method "getFinalAmount()" implemented which will return the final checkout cost. The same is called from the checkout method.
    Tested on postman and got correct outputs.

--- ecommerce_api/src/main/java/com/assignment/rishita/ecommerce_api/RestController1.java
+++ ecommerce_api/src/main/java/com/assignment/rishita/ecommerce_api/RestController1.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 //java.util imports
 import java.util.List;
+import java.util.HashMap;
 
 //other imports
 import org.springframework.http.MediaType;
@@ -29,9 +30,56 @@ public class RestController1 {
 	}
 	@PostMapping(value = "/checkout",  consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public int checkout(@RequestBody List<String> IDs){
-		//TODO: call method which will calculate total price and return
-        return 0;
+            int price = getFinalAmount(IDs);
+            return price;
 	}
+
+
+	int getFinalAmount(List<String> IDs){
+       
+        //po (purchase order) hashmap will contain the watch IDs as key and quantity ordered as values.
+        HashMap<String,Integer> po = new HashMap<String,Integer>(); 
+
+        // loop through all IDs input and update the quantity in po hashmap
+        for(int i=0; i<IDs.size(); i++){
+            if(po.containsKey(IDs.get(i))==false){
+                po.put(IDs.get(i),1);
+            }
+			else{
+            	po.put(IDs.get(i),po.get(IDs.get(i))+1);
+			}
+        }
+
+        //calculate final price- loop through all IDs in po hashmap and take necessary steps 
+        int price = 0;
+
+        for(String id: po.keySet()){
+
+            //get Database table record values for that ID
+           Watch a = WatchRepository.getById(id);
+           int discountQuantity = a.getDiscountQuantity();
+           int unitPrice = a.getUnitPrice();
+
+            // get quantity to be purchased from po hashmap
+           int quantityToBePurchased = po.get(id);
+
+            // if discount applies on this WatchID, then increment total price accordingly
+           if(discountQuantity!=0 && quantityToBePurchased>=discountQuantity){
+                int discountVal = a.getDiscountVal();
+                while(quantityToBePurchased>=discountQuantity){
+                    price += discountVal;
+                    quantityToBePurchased -= discountQuantity;
+                }
+           }
+
+           
+           price += (quantityToBePurchased * unitPrice);
+
+        }
+       
+    
+        return price;
+    }   
 
 
 
